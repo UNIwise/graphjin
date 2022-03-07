295542ec57c8dfeba7be73cfc63611e7c2522417
Andreas <git@xar.io>
2022-03-07 00:29:55 -0800

    config now reloads on file changes in the config dir (#6)
    * config now reloads on file changes in the config dir
    
    * Update core/watcher.go
    
    Co-authored-by: Jonas Tranberg <naueramant@users.noreply.github.com>
    
    * Update core/watcher.go
    
    Co-authored-by: Jonas Tranberg <naueramant@users.noreply.github.com>
    
    Co-authored-by: Jonas Tranberg <naueramant@users.noreply.github.com>

--- core/watcher.go
+++ core/watcher.go
@@ -1,9 +1,12 @@
 package core
 
 import (
+	"fmt"
 	"time"
 
+	"github.com/bep/debounce"
 	"github.com/dosco/graphjin/core/internal/sdata"
+	"github.com/fsnotify/fsnotify"
 )
 
 func (g *GraphJin) initDBWatcher() error {
@@ -58,4 +61,59 @@ func (g *GraphJin) startDBWatcher(ps time.Duration) {
 			gj.log.Println(err)
 		}
 	}
+}
+
+func (g *GraphJin) initFSWatcher() error {
+	gj := g.Load().(*graphjin)
+
+	go func() {
+		watcher, err := fsnotify.NewWatcher()
+		if err != nil {
+			gj.log.Printf("Error during watcher initialization: %s", err)
+			return
+		}
+
+		watchPaths := []string{
+			gj.conf.ConfigPath,
+			fmt.Sprintf("%s/fragments/", gj.conf.ConfigPath),
+			fmt.Sprintf("%s/queries/", gj.conf.ConfigPath),
+			fmt.Sprintf("%s/scripts/", gj.conf.ConfigPath),
+		}
+
+		for _, path := range watchPaths {
+			err = watcher.Add(path)
+			if err != nil {
+				gj.log.Printf("Failed adding watch path: %s", path)
+				return
+			}
+		}
+
+		invokeReload := func() {
+			gj.log.Println("Reloading config due to one or more file changes...")
+			err = g.Reload()
+			if err != nil {
+				gj.log.Println("Failed to reload config during FS change: ", err)
+			}
+		}
+
+		debounced := debounce.New(10 * time.Second)
+
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				gj.log.Printf("%s %s\n", event.Name, event.Op)
+				debounced(invokeReload)
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				gj.log.Println("error:", err)
+			}
+		}
+	}()
+
+	return nil
 }
